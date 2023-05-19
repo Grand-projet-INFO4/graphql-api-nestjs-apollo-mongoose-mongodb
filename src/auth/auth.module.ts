b28e4a2261fec688fdc08c0053ad8ef6ff74f0f1
Tolotra <tolotor@yahoo.fr>
2023-05-19 08:40:32 +0300

    Additional setup and cleaning

--- src/auth/auth.module.ts
+++
@@ -1,8 +0,0 @@
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { Module } from '@nestjs/common';
-@Module({
-    controllers: [AuthController],
-    providers:[AuthService]
-})
-export class AuthModule {}
