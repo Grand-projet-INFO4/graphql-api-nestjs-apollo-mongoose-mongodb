b219fd0fcbfcba0479a9fe79318d30eca7cb1dc9
Tolotra <tolotor@yahoo.fr>
2023-08-30 00:01:27 +0300

    Extra utils types

--- src/common/types/utils.d.ts
+++ src/common/types/utils.d.ts
@@ -1,3 +1,14 @@
 // Replaces the types of the fields of a given type with new ones
 export type ReplaceFields<T, TReplacement> = Omit<T, keyof TReplacement> &
   TReplacement;
+
+export type RemoveMethods<T> = Pick<
+  T,
+  Exclude<
+    {
+      // eslint-disable-next-line @typescript-eslint/ban-types
+      [key in keyof T]: T[key] extends Function ? never : key;
+    }[keyof T],
+    undefined
+  >
+>;
